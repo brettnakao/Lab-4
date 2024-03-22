2987e573225d4500fbc31efdf68e2e5e50910420
brettnakao <bnakao@lion.lmu.edu>
2024-03-21 18:49:35 -0700

    small edit

--- Example 1- Calculating Gravitational Force between Planets.py
+++ Example 1- Calculating Gravitational Force between Planets.py
@@ -8,7 +8,8 @@ Created on Sat Mar  2 18:33:00 2024
 
 import numpy as np
 
-G = 6.674e-11
+# Constant
+G = 6.674e-11 # Gravitational constant (N m^2/kg^2)
 
 masses = np.array([1.0241e26, 8.6810e25, 5.6834e26, 1.8982e27, 6.4171e23, 5.972e24, 4.8675e24, 3.3011e3])
 distance = np.array([4.5e12, 2.9e12, 1.4e12, 7.78e11, 2.28e11, 1.496e11, 1.08e11, 5.79e10])
